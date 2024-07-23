dfd4d350087193c641d9e2e1998ef170cd7395b8
Lucaspmsp <166307119+Lucaspmsp@users.noreply.github.com>
2024-07-22 22:37:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
 AOS.init();
 
-
-
-
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    var comprarProdutoLink = document.getElementById('comprar-produto');
-    
-    if (comprarProdutoLink) {
-        comprarProdutoLink.addEventListener('click', function(event) {
-            // Coloque aqui o código de rastreamento do Facebook
-            fbq('track', 'InitiateCheckout');
-            
-            // Certifique-se de que o link seja seguido após o rastreamento
-            // Aqui, cancelamos o comportamento padrão do link para este exemplo
-            event.preventDefault();
-            
-            // Você pode redirecionar o usuário para o URL real depois do rastreamento
-            // window.location.href = comprarProdutoLink.href;
-        });
-    }
-});
-</script>
